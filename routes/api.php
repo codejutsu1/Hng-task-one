d8888fdf56253616a60d931a4fe6675d72a9e2ad
codejutsu1 <codejutsu@protonmail.com>
2023-09-07 14:44:17 +0100

    temporary removed the api prefix for the project

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/', [TaskController::class, 'index']);
+Route::get('/api', [TaskController::class, 'index']);
