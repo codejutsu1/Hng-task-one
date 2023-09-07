dd1f667d2488cf7c4432293f07fbff5970281cbf
codejutsu1 <codejutsu@protonmail.com>
2023-09-07 14:20:48 +0100

    Completed the task

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     public function index(Request $request)
     {
-        return response()->json($request->query());
+        return response()->json([
+            "slack_name" => $request->query('slack_name'),
+            "current_day" => Carbon::today()->englishDayOfWeek,
+            "utc_time" => Carbon::now('UTC'),
+            "track" =>  $request->query('track'),
+            "github_file_url" => "https://github.com/codejutsu1/Hng-task-one/blob/main/app/Http/Controllers/TaskController.php",
+            "github_repo_url" => "https://github.com/codejutsu1/Hng-task-one",
+            "status_code" => 200
+        ], 200);
     }
 }
